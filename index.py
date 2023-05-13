24c315723bbc7e73946d92ea0a51a45adc561251
draJiang <598353451@qq.com>
2023-05-13 23:19:45 +0800

    can customize whiteboard ids

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import time
 import json
@@ -7,16 +7,19 @@ from flask_cors import CORS
 HEPTABASE_WHITEBOARD_ID = 'd4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884'
 
 # 存储 heptabase base 数据
-HEPTABASE_DATA = {'result':'erro','data':{},'time':''}
+HEPTABASE_DATA = {'result': 'erro', 'data': {}, 'time': ''}
 
-def get_hepta_data():
+
+def get_hepta_data(whiteboard_id):
     '''
     获取 heptabase 数据
     '''
+
     req = requests.get(
-        'https://app.heptabase.com/api/whiteboard/?secret='+HEPTABASE_WHITEBOARD_ID)
+        'https://app.heptabase.com/api/whiteboard/?secret='+whiteboard_id)
     req_json = json.loads(req.text)
     return req_json
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -25,15 +28,18 @@ CORS(app, supports_credentials=True)
 @app.route('/')
 def home():
     global HEPTABASE_DATA
+
+    whiteboard_id = request.args.get('whiteboard_id')
     
-    if(HEPTABASE_DATA['result']!='success'):
-        # 如果全局变量中没有存储数据
-        req_json = get_hepta_data()
-        HEPTABASE_DATA = {'result':'success','data':req_json,'time':int(time.time())}
-        return HEPTABASE_DATA
+    if(whiteboard_id):
+        req_json = get_hepta_data(whiteboard_id)
     else:
-        # 全局变量有存储数据则直接返回，以提升数据获取速度。
-        return HEPTABASE_DATA
+        req_json = get_hepta_data(HEPTABASE_WHITEBOARD_ID)
+
+    HEPTABASE_DATA = {'result': 'success',
+                      'data': req_json, 'time': int(time.time())}
+    return HEPTABASE_DATA
+
 
 @app.route('/update')
 def update():
@@ -42,8 +48,10 @@ def update():
     '''
     global HEPTABASE_DATA
     req_json = get_hepta_data()
-    HEPTABASE_DATA = {'result':'success','data':req_json,'time':int(time.time())}
+    HEPTABASE_DATA = {'result': 'success',
+                      'data': req_json, 'time': int(time.time())}
     return HEPTABASE_DATA
+
 
 @app.route('/about')
 def about():
@@ -65,6 +73,7 @@ def api():
     with open('data.json', mode='r') as my_file:
         text = my_file.read()
         return text
+
 
 if __name__ == '__main__':
     app.run(debug=True)
